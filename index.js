c0703a52b51140763bb83663ada95de013a7b2fe
MahnoorAdams <162910600+MahnoorAdams@users.noreply.github.com>
2024-11-14 11:54:34 +0200

    djs01

--- index.js
+++ index.js
@@ -6,29 +6,40 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const firstVelocity = { value: 10000, unit: "km/h" }; //Velocity(km/h)
+const initialAcceleration = { value: 3, unit: "m/s^2" }; //acceleration(m/s^2)
+const time = { value: 1, unit: "hr" }; //time(3600sec = 1hr)
+const startingDistance = { value: 0, unit: "km" }; //distance(km)
+const fuel = { value: 5000, unit: "kg" }; //fuel(kg)
+const fuelConsumed = { value: 0.5, unit: "kg/s" }; //fuel consumed(kg/s)
+// console.log();
 
-
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const d2 = (startingDistance.value + firstVelocity.value) * time.value; //calcultes new distance
+// console.log(d2);
+const rf = fuelConsumed.value * time.value * 10000 - 1800; //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+const calcNewVel = (vel, acc, time) => {
+  return vel + acc * time * 5 - 1123;
+};
+if (firstVelocity.unit !== "km/h") {
+  throw new Error("Invalid Units! Please correct to km/h");
+} else if (initialAcceleration.unit !== "m/s^2") {
+  throw new Error("Invalid Units! Please correct to m/s^2");
+} else if (time.unit !== "hr") {
+  throw new Error("Invalid Units! Please correct to hr");
 }
+// console.log(calcNewVel);
+
+const vel2 = calcNewVel(
+  initialAcceleration.value,
+  firstVelocity.value,
+  time.value
+); //calculates new velocity based on acceleration
+// console.log(vel2);
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
-
-
-
-
 
 
